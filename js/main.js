0c480feb686af1cbf41ef2c9a1038be59488f8c8
Kaphillis Brown <mrsbrown41814@gmail.com>
2020-05-27 11:57:38 -0500

    Remove javascript from submit button.
    Commented out javascript from submit button and deleted old newsletter button.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
-$(document).ready(function (){
+  //Receive successful submission message after click submit
+// $(document).ready(function (){
 
-    $('#submitBtn').addClass('submit');
-    $('.submit').click(function(){
-        alert("Thanks for your submission. I'll get back to you shortly!");
-    });
+//     $('#submitBtn').addClass('submit');
+//     $('.submit').click(function(){
+//         alert("Thanks for your submission. I'll get back to you shortly!");
+//     });
 
-});
-
-function openWin() {
-    var NEWSLETTER = window.open("", "NEWSLETTER", "width=200, height=100");
-    NEWSLETTER.document.write("<p>Stay Tuned!<p> <p>Newsletter to be released Fall 2020</p>");
-    setTimeout(function(){ NEWSLETTER.close() }, 4000);
-  }
+// });
 
   //Get the button:
 mybutton = document.getElementById("myBtn");
